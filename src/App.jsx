3c6d2e4168373f0f1f421d1260b26ea8bfd81af7
Chillie <reskialamsyah@gmail.com>
2026-02-27 01:03:47 +0800

    UPDATE: add sidebar into app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/navbar/Navbar";
+import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
   return (
     <div className="flex min-h-screen min-w-screen flex-col">
       <Navbar></Navbar>
+      <Sidebar></Sidebar>
     </div>
   );
 }
