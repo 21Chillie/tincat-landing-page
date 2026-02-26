c20e04834d4d20779d3240c4f6f24c82d3b3592c
Chillie <reskialamsyah@gmail.com>
2026-02-26 21:15:12 +0800

    UPDATE: add navbar into app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
-function App() {}
+import Navbar from "./components/navbar/Navbar";
+
+function App() {
+  return (
+    <div className="flex min-h-screen min-w-screen flex-col">
+      <Navbar></Navbar>
+    </div>
+  );
+}
 
 export default App;
