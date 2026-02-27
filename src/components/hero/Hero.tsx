f425d415e8ef308cb3d683d2b681cf26eb464eab
Chillie <reskialamsyah@gmail.com>
2026-02-28 00:42:02 +0800

    UPDATE: hero section component

---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,58 @@
+import heroImg from "../../assets/images/heroes-section/hero-img.webp";
+import { RiStarSFill } from "react-icons/ri";
+
+function Hero() {
+  return (
+    <>
+      <section
+        id="hero-section"
+        className="bg-base-200 px-4 pt-12 pb-12 md:pt-16 md:pb-0"
+      >
+        <header className="mx-auto grid max-w-7xl grid-cols-1 md:grid-cols-2">
+          <div className="flex flex-col justify-center text-center md:text-left">
+            <h1 className="text-base-content mb-6 text-5xl font-extrabold">
+              Meet The Purr-fect Matchmakers
+            </h1>
+            <p className="text-base-content/70 text-lg">
+              In a world where every whisker tells a tale, our determined feline
+              heroes are on a mission to make tails wag and hearts purr! They
+              are the love champions, whiskered wingmen, and supreme matchmakers
+              in the cat kingdom.
+            </p>
+
+            <div className="mt-6 flex flex-col items-center gap-4 md:flex-row">
+              <button
+                className="btn btn-circle btn-wide btn-accent text-base-200 text-lg font-bold"
+                type="button"
+              >
+                Get Started
+              </button>
+
+              <div className="flex flex-col items-center md:items-start">
+                <div className="flex items-center gap-2">
+                  <div className="flex items-center text-yellow-600">
+                    {Array.from({ length: 5 }).map((_, i) => {
+                      return <RiStarSFill key={i}></RiStarSFill>;
+                    })}
+                  </div>
+                  <span className="text-base font-bold">5.0</span>
+                </div>
+
+                <p className="text-base">
+                  from 1000+
+                  <span className="font-bold underline"> reviews</span>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <figure className="hidden justify-center md:flex">
+            <img className="md:max-w-64 lg:max-w-72" src={heroImg} alt="" />
+          </figure>
+        </header>
+      </section>
+    </>
+  );
+}
+
+export default Hero;
