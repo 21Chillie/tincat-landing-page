ed7d22d314ff1caa458b9bda7b9a85888cf70b01
Chillie <reskialamsyah@gmail.com>
2026-02-28 00:42:19 +0800

    ADD: sponsor section component (not finish)

---
+++ src/components/sponsor/Sponsor.tsx
@@ -0,0 +1,14 @@
+function Sponsor() {
+  return (
+    <>
+      <section
+        id="sponsor-section"
+        className="border-base-content/30 mx-auto max-w-7xl border-y py-6"
+      >
+        <h1>Hello</h1>
+      </section>
+    </>
+  );
+}
+
+export default Sponsor;
