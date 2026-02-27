a86290fb5732492f57f0b8784994614cca624d0f
Chillie <reskialamsyah@gmail.com>
2026-02-27 21:17:35 +0800

    UPDATE: change to typescript and fix error

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
-import App from "./App.jsx";
-import { AppProvider } from "./contexts/globalContext.jsx";
+import App from "./App.js";
+import { AppProvider } from "./contexts/globalContext.js";
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AppProvider>
       <App />
