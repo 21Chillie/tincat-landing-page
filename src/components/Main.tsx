61fbded4045e7e0ee654e7cb51f024222758b0cc
Chillie <reskialamsyah@gmail.com>
2026-02-28 00:41:20 +0800

    ADD: main section component

---
+++ src/components/Main.tsx
@@ -0,0 +1,15 @@
+import Hero from "./hero/Hero";
+import Sponsor from "./sponsor/Sponsor";
+
+function Main() {
+  return (
+    <>
+      <main className="">
+        <Hero></Hero>
+        <Sponsor></Sponsor>
+      </main>
+    </>
+  );
+}
+
+export default Main;
