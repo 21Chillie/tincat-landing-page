5539b4b43cd4e4bc3674577df5f6b1cee4217946
Chillie <reskialamsyah@gmail.com>
2026-02-28 03:45:16 +0800

    UPDATE: sponsor section component

--- src/components/sponsor/Sponsor.tsx
+++ src/components/sponsor/Sponsor.tsx
@@ -1,11 +1,31 @@
+import { sponsors } from "../../contexts/sponsor";
+
 function Sponsor() {
   return (
     <>
       <section
         id="sponsor-section"
-        className="border-base-content/30 mx-auto max-w-7xl border-y py-6"
+        className="bg-base-200 border-base-content/30 mx-auto hidden border-y p-4 py-6 md:block"
       >
-        <h1>Hello</h1>
+        <div className="mx-auto flex max-w-7xl items-center justify-between gap-4">
+          <p className="font-bold">More than 100+ companies partner</p>
+          <ul className="grid grid-cols-4 place-items-center gap-4">
+            {sponsors.map((sponsor, index) => {
+              const { name, imgSrc } = sponsor;
+              return (
+                <li key={index}>
+                  <div className="bg-base-100 hover:bg-base-300 hover:outline-base-content/30 t flex h-32 w-32 items-center justify-center rounded-full p-4 transition-all hover:shadow-lg">
+                    <img
+                      className="max-w-24"
+                      src={imgSrc}
+                      alt={`${name} logo`}
+                    />
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </section>
     </>
   );
