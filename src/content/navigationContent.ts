a573dad1dc202f93d0194e5de5dbb19872281485
Chillie <reskialamsyah@gmail.com>
2026-02-28 23:18:19 +0800

    UPDATE: config for set navbar and sidebar content

---
+++ src/content/navigationContent.ts
@@ -0,0 +1,21 @@
+export type linksType = {
+  text: string;
+};
+
+export const links: linksType[] = [
+  {
+    text: "home",
+  },
+  {
+    text: "products",
+  },
+  {
+    text: "about",
+  },
+  {
+    text: "support",
+  },
+  {
+    text: "download",
+  },
+];
