4796908fcc5bb3675ce3abd6b63f9f8f510bc91c
Chillie <reskialamsyah@gmail.com>
2026-02-28 23:17:48 +0800

    UPDATE: config for set feature section content

---
+++ src/content/featureContent.ts
@@ -0,0 +1,12 @@
+type featureHeaderType = {
+  label: string;
+  title: string;
+  description: string;
+};
+
+export const featureHeader: featureHeaderType = {
+  label: "CAT MATCHING",
+  title: "Purr-fect connections that grow with every swipe.",
+  description:
+    "Find your feline soulmate with a platform built for cats and the humans who love them from curious kittens to seasoned nappers, there's a perfect match for every whisker.",
+};
