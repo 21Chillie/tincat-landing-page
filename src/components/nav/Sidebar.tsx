7f4ee0627af7dae561ea678b46038f904097d588
Chillie <reskialamsyah@gmail.com>
2026-03-03 05:41:32 +0800

    UPDATE: fix sidebar layout and add sub link

--- src/components/nav/Sidebar.tsx
+++ src/components/nav/Sidebar.tsx
@@ -10,14 +10,16 @@ function Sidebar() {
   return (
     <>
       <div
-        className={`fixed inset-0 z-3 bg-black/30 transition-opacity duration-300 ${isSidebarOpen ? "opacity-100" : "pointer-events-none block opacity-0 md:hidden"}`}
+        className={`fixed inset-0 z-3 block bg-black/30 transition-opacity duration-300 lg:hidden ${
+          isSidebarOpen ? "opacity-100" : "pointer-events-none opacity-0"
+        }`}
         onClick={toggleSidebar}
       ></div>
 
       <aside
-        className={`${isSidebarOpen ? "translate-x-0" : "-translate-x-full"} bg-base-100 fixed z-3 block h-screen transition-transform md:hidden`}
+        className={`${isSidebarOpen ? "translate-x-0" : "-translate-x-full"} bg-base-100 fixed z-3 block h-screen transition-transform lg:hidden`}
       >
-        <nav className="p-6">
+        <nav className="h-full overflow-y-auto p-4 md:p-6">
           <div className="mb-4 flex items-center gap-2">
             <div className="bg-base-300 w-fit rounded-full p-1 shadow-sm">
               <img className="h-8 w-8" src={brandIconSrc} alt={brandIconAlt} />
@@ -26,22 +28,41 @@ function Sidebar() {
             <span className="text-3xl font-bold">{brandName}</span>
           </div>
 
-          <ul>
-            {links.map((link) => {
-              const { text } = link;
+          <div role="menu">
+            <ul className="space-y-4">
+              {links.map((link) => {
+                const { heading, links } = link;
 
-              return (
-                <li className="w-52" key={nanoid()}>
-                  <a
-                    className="active:bg-accent active:text-accent-content block rounded-full py-2 capitalize transition-all active:px-4"
-                    href="#"
-                  >
-                    {text}
-                  </a>
-                </li>
-              );
-            })}
-          </ul>
+                return (
+                  <li className="w-52 md:w-64" key={nanoid()}>
+                    <header className="mb-1">
+                      <p className="text-lg font-medium capitalize md:font-bold">
+                        {heading}
+                      </p>
+                    </header>
+
+                    <div role="menu">
+                      <ul className="">
+                        {links.map((sub) => {
+                          const { href, text } = sub;
+                          return (
+                            <li key={text}>
+                              <a
+                                className="text-accent hover:bg-accent-content block rounded-full py-1 transition-all hover:px-3"
+                                href={href}
+                              >
+                                {text}
+                              </a>
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </nav>
       </aside>
     </>
