13ff7ce790ad68c8018bf00c2c6b3262378d8a7d
Chillie <reskialamsyah@gmail.com>
2026-03-03 05:44:22 +0800

    UPDATE: add sublink menu into main app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Main from "./components/Main";
 import Navbar from "./components/nav/Navbar";
 import Sidebar from "./components/nav/Sidebar";
+import { SubLink } from "./components/nav/SubLink";
 
 function App() {
   return (
     <div className="flex min-h-screen min-w-screen flex-col">
       <Navbar></Navbar>
+      <SubLink></SubLink>
       <Sidebar></Sidebar>
       <Main></Main>
     </div>
